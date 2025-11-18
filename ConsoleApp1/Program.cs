1740453a2d568f2d0d2a87518e742325dc3347d9
Gustav <gustavjohnsson2008@gmail.com>
2025-11-18 10:15:57 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Spel ide, Spelaren är en äventyrare som ska ge sig in i den ny världen och bli starkare så de kan skydda det som de bryr sig om
+// Spelet börjar med att vi får veta lite om äventyraren och historian bakom denna värld man ger sig in i och staden, Elysium.
+// Spelet börjar
+
+// Del 1.
+
+// Splearen får välja mellan mage och swordsman.
+// Spelaren får välja namn på sin karaktär 
+// Spelaren går till sin class guild house och ska få sina abilities och skills.
+// Slumpar mellan en lista på abilities beroende på den klass man väljer.
+// Går till quest boarden som finns i guild houses och får 3 options.
+// Quest 1: Pick 5 Rose lilies in the fields of Elysium.
+// Quest 2: Defeat 3 wolves, found in the Shadowfen forsest.
+// Quest 3: Find the treasure in the cave of radiance.
+// Spelaren väljer vilken quest de vill ha och får se infon om vilka rewards man får av questen.
+// Man blir teleporterad till questen.
